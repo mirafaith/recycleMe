8ca41aa16766fe9b5e5a2a4695ad3f24c9ed7df5
Ningshun Chen <ningshunchen@gmail.com>
2020-04-25 18:07:36 -0400

    connect.php successed!

--- connect.php
+++ connect.php
@@ -6,13 +6,13 @@
 $username = 'root';
 $password = 'RecycleMe?1234';
 
-$dbname = 'recycleme-db';
+$dbname = 'RecycleMe';
 
 // if PHP is on GCP standard App Engine, use instance name to connect
-$host = 'recycleme-275020:us-east4:recycleme-db';
+// $host = 'recycleme-275020:us-east4:recycleme-db';
 
 // if PHP is hosted somewhere else (non-GCP), use public IP address to connect
-// $host = "public-IP-address-to-cloud-instance";
+$host = "35.194.84.221";
 
 /******************************/
 
